29d46be1c1f6cf338c77d8af16e2d58af188a5b1
Alibentiba <bentibaali3@example.com>
2022-11-20 13:21:40 +0100

    'refactor-app'

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,22 @@ import './App.css'
 import Feed from './Feed/Feed'
 import Login from './Login/Login'
 import { useState,useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import {LoginA} from './Re/Slice'
 
 
 function App() {
-// const [user1,setuser]=useState(null)
-
+  const dispatch=useDispatch()
+useEffect(()=>{
+  const v = JSON.parse(localStorage.getItem('x'));
+if(v){
+  dispatch(LoginA(v))
+  console.log('dsfsdfsfsd',v)
+}},[])
 var user1=useSelector(state=>state.userstore.user)
-
-
 console.log("user1of local",user1);
     return (
-      <div className='App'>
-         
-         
+      <div className='App'>   
  {user1?.email? ( 
 
 
